973d1ec1b54117be8cdfea7a037ffbed9e159ab7
MrTreys <omedvedev6666@gmail.com>
2024-02-17 21:54:26 +0800

    Array writer added

--- Program.cs
+++ Program.cs
@@ -24,3 +24,12 @@ for(int i = 0; i < newArray.Length; i++){
 }
 return newArray;
 }
+void Write1dArray(string[] array){
+    for(int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+string[] array = GetArray();
+Console.WriteLine("Получлся вот такой массив: ");
+Write1dArray(RuinArray(array));
