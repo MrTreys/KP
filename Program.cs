d6ebedaf2564b8e996366d0974abeb27a10150aa
MrTreys <omedvedev6666@gmail.com>
2024-02-17 21:50:46 +0800

    Array asking added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random Any = new Random();
+string[] GetArray(){
+    string[] array = new string[Any.Next(2, 6)];
+    Console.WriteLine($"Задайте массив, состоящий из строк и объёмом {array.Length}: ");
+    for(int i = 0; i < array.Length; i++){Console.Write($"Элемент {i+1}: "); array[i] = Console.ReadLine()!;}
+    return array;
+}
