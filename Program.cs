9fd4248eba98f6cd82814f485035e951adcb4efd
MrTreys <omedvedev6666@gmail.com>
2024-02-17 21:52:19 +0800

    Array converter added

--- Program.cs
+++ Program.cs
@@ -5,3 +5,22 @@ string[] GetArray(){
     for(int i = 0; i < array.Length; i++){Console.Write($"Элемент {i+1}: "); array[i] = Console.ReadLine()!;}
     return array;
 }
+string[] RuinArray(string[] array){
+int len;
+if(array.Length <= 3){len = array.Length;}
+else{len = 3;}
+string[] newArray = new string[Any.Next(0, len)];
+for(int i = 0; i < newArray.Length; i++){
+    int s = Any.Next(0, array.Length);
+    string expect = "";
+    bool validate = false;
+    for(int j = 0; j < expect.Length; j++){
+        if(expect[j] == s){validate = true; break;}
+    }
+    if(validate == false){
+        newArray[i] = array[s];
+        expect += s;
+    }
+}
+return newArray;
+}
